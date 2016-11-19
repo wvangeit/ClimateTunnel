de1307410a8b74b70702a6009b66154acb4dcdfc
Werner Van Geit <werner.vangeit@epfl.ch>
2016-11-19 02:26:24 +0100

    Added polar plot of global sea ice area

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
+# Sea Ice
+
+![Global sea ice area monthly, polar plot](https://github.com/wvangeit/ClimateTunnel/blob/master/gifs/seaice.gif)
+
+Polar plot of global sea ice area (land and sea surface) for the period 1978-2016.
+
+Bcenter and circumference of plotting area represent 12 - 24 million km2 respectively.
+
+All data obtained from [ArtischePinguin](https://sites.google.com/site/arctischepinguin/home), [raw file](https://sites.google.com/site/arctischepinguin/home/sea-ice-extent-area/data/nsidc_global_nt_final_and_nrt.txt.gz)
+
+
 # ClimateTunnel
 
 ![Global-mean monthly, polar plot](https://github.com/wvangeit/ClimateTunnel/blob/master/gifs/climate.gif)
@@ -7,3 +18,4 @@ Polar plot of global mean monthly temperature anomaly (land and sea surface) for
 Black circle represent 0 anomaly, center and circumference of plotting area represent -/+ 1.5 degrees Celsius anomalies respectively.
 
 All data obtained from [GISS Surface Temperature Analysis website](http://data.giss.nasa.gov/gistemp/)
+
---
+++ seaicetunnel.py
@@ -0,0 +1,134 @@
+"""Polar plot of temperature"""
+
+# pylint: disable=E1103
+
+import os
+import datetime
+
+import numpy
+import pandas
+
+
+import matplotlib.animation as animation
+import matplotlib.collections as collections
+import matplotlib.pyplot as plt
+
+plt.style.use('ggplot')
+
+data_dir = 'data'
+
+csv_filename = os.path.join(data_dir, 'nsidc_global_nt_final_and_nrt.txt')
+
+data = pandas.read_csv(
+    csv_filename,
+    comment='#',
+    infer_datetime_format=True,
+    parse_dates=[0],
+    skipinitialspace=True)
+
+dates_without_year = [
+    datetime.date(
+        1980,
+        x.month,
+        x.day) for x in data.loc[:, 'date']]
+
+years = [x.year for x in data.loc[:, 'date']]
+unique_years = [
+    years[i] for i in range(len(years)) if i == years.index(years[i])]
+years = numpy.array(years)
+
+days_from_1jan = [(x.date() - datetime.date(x.year, 1, 1)).days
+                  for x in data.loc[:, 'date']]
+thetas = numpy.array([float(x) / 365 * 2 * numpy.pi
+                      for x in days_from_1jan])
+
+areas = data['area'].as_matrix()
+
+fig = plt.figure(figsize=(12, 12))
+
+ax = fig.add_subplot(1, 1, 1, projection='polar')
+
+line = collections.LineCollection(
+    [],
+    linewidth=10,
+    alpha=.2,
+    cmap=plt.get_cmap('cool'),
+    norm=plt.Normalize(
+        12,
+        30))
+
+ax.add_collection(line)
+
+title = ax.text(-0.11, 0.0, '', fontsize=50, transform=ax.transAxes)
+caption1 = ax.text(-0.11,
+                   1.1,
+                   'Global sea ice area (M km2)',
+                   fontsize=25,
+                   transform=ax.transAxes)
+caption2 = ax.text(-0.11,
+                   1.025,
+                   'data: https://sites.google.com/site/arctischepinguin/\n'
+                   'code: https://github.com/wvangeit/ClimateTunnel',
+                   fontsize=10,
+                   transform=ax.transAxes)
+
+
+ax.set_ylim([12, 24])
+ax.set_xticks(2.0 * numpy.pi * numpy.linspace(0, 1, 12, endpoint=False))
+ax.set_xticklabels(['Jan',
+                    'Feb',
+                    'Mar',
+                    'Apr',
+                    'May',
+                    'Jun',
+                    'Jul',
+                    'Aug',
+                    'Sep',
+                    'Oct',
+                    'Nov',
+                    'Dec'], fontsize=20)
+
+ax.set_yticks([12, 18, 24])
+ax.set_yticklabels(['12', '18', '24 M Km2'], fontsize=20)
+
+
+def init():
+    """Init"""
+
+    line.set_segments([])
+    title.set_text('')
+    return line, title
+
+
+def animate(t):
+    """Animate"""
+
+    if t < len(unique_years):
+        t_year = unique_years[t]
+
+        print t_year
+
+        t_thetas = thetas[numpy.where(years <= t_year)]
+        t_rs = areas[numpy.where(years <= t_year)]
+
+        points = numpy.array([t_thetas, t_rs]).T.reshape(-1, 1, 2)
+        segments = numpy.concatenate([points[:-1], points[1:]], axis=1)
+
+        title.set_text(str(t_year))
+        line.set_segments(segments)
+        line.set_array(t_rs)
+        # marker.set_data(t_thetas, t_rs)
+
+    return line, title
+
+ani = animation.FuncAnimation(
+    fig,
+    animate,
+    numpy.arange(len(unique_years) + 20),
+    init,
+    interval=200,
+    blit=False,
+    repeat=False)
+
+ani.save('gifs/seaice.gif', dpi=80, writer='imagemagick')
+# plt.show()
